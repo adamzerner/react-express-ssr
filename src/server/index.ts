8ebc0ece4ad6fd98dbf7952907a944140832b5cf
Adam Zerner <adamzerner@protonmail.com>
2022-10-30 18:11:11 -0700

    Make variable more local

--- src/server/index.ts
+++ src/server/index.ts
@@ -14,10 +14,6 @@ export const createServer = async (
 ) => {
   const resolve = (p: string) => path.resolve(__dirname, p);
 
-  const indexProd = isProd
-    ? fs.readFileSync(resolve("dist/client/index.html"), "utf-8")
-    : "";
-
   const app = express();
   const viteServer = await setup(app, root, isProd, isTest, hmrPort);
 
@@ -33,6 +29,10 @@ export const createServer = async (
         render = (await viteServer.ssrLoadModule("/src/entry-server.tsx"))
           .render;
       } else {
+        const indexProd = isProd
+          ? fs.readFileSync(resolve("dist/client/index.html"), "utf-8")
+          : "";
+
         template = indexProd;
         // @ts-expect-error The file will exist after running `npm run build`.
         render = (await import("./dist/server/entry-server")).render;
